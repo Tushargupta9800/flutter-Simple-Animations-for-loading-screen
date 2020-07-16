9224f38c5ce11307b657e36e3db34bd0c67cba81
Tushar Gupta <58504726+Tushargupta9800@users.noreply.github.com>
2020-07-16 18:06:56 +0530

    Added loading4

---
+++ loading4.dart
@@ -0,0 +1,73 @@
+////////////////////////////////////////////////////////////
+///                 - Tushar Gupta                       ///
+////////////////////////////////////////////////////////////
+
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+AnimationController animationController;
+Animation animation;
+Tween tween;
+
+class _HomeState extends State<Home> with TickerProviderStateMixin{
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(vsync: this, duration: Duration(seconds: 1));
+    tween = Tween<double>(begin: 0.0,end: 1.0);
+    animation = tween.animate(animationController);
+    animation.addListener(() {setState(() {});});
+    animation.addStatusListener((status) {
+      if(status == AnimationStatus.completed){
+        animationController.reverse();
+      }else if(status == AnimationStatus.dismissed){
+        animationController.forward();
+      }
+    });
+    animationController.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        color: Colors.black,
+        child: CustomPaint(
+          painter: iwanttopaint(),
+        )
+      ),
+    );
+  }
+}
+
+class iwanttopaint extends CustomPainter{
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var path = Path();
+    path.moveTo(0, size.height);
+    path.lineTo(0, size.height * 0.92);
+    path.quadraticBezierTo(size.width * 0.25, size.height + (-120 + animation.value*100), size.width * 0.5, size.height * 0.92);
+    path.quadraticBezierTo(size.width * 0.75, size.height  - animation.value*100, size.width , size.height * 0.92);
+    path.lineTo(size.width, size.height);
+    var paint = Paint();
+    paint.color = Colors.yellowAccent;
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
+
+}
